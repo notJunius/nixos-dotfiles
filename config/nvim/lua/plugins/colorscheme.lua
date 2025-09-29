741258f35f2c92414c52a9b0233fb48bbae74d4d
notJunius <120129256+notJunius@users.noreply.github.com>
2025-09-29 14:46:00 -0600

    added new colors and walls for everforest

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,8 @@ return {
 	-- Optional; default configuration will be used if setup isn't called.
 	config = function()
 		require("everforest").setup({
+			background = "medium",
+			transparent_background_level = 1,
 		})
 		vim.cmd("colorscheme everforest")
 	end,
